9b16399c1ae00428f051acd273cafe60cbf4571a
mar8us <gordonsix0@gmail.com>
2025-01-08 19:31:19 +0100

    Connect toolbar settings action with StackedWidget navigation

--- main_window.cpp
+++ main_window.cpp
@@ -6,9 +6,37 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connectButtons();
+    setIcons();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::navigateToToolBoxPage(QWidget* page)
+{
+    if(page && ui->stackedWidget->indexOf(page) != -1)
+        ui->stackedWidget->setCurrentWidget(page);
+}
+
+void MainWindow::updateActionIcons(int index)
+{
+    ui->actionSettings->setIcon(ui->stackedWidget->widget(index) == ui->settingsPage ? activeSettingsIcon : defaultSettingsIcon);
+}
+
+void MainWindow::connectButtons()
+{
+    connect(ui->actionSettings, &QAction::triggered, this, [this]() {
+        navigateToToolBoxPage(ui->settingsPage);
+    });
+
+    connect(ui->stackedWidget, &QStackedWidget::currentChanged, this, &MainWindow::updateActionIcons);
+}
+
+void MainWindow::setIcons()
+{
+    defaultSettingsIcon = QIcon(":/icons/settings_white.png");
+    activeSettingsIcon = QIcon(":/icons/settings_selected.png");
 }
