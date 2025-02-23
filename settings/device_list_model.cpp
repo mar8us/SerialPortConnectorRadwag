afdcd408618877005b6cfff776d8194d4c66164c
mar8us <gordonsix0@gmail.com>
2025-02-23 18:09:10 +0100

    Refactor of DeviceListModel - moved methods
    Method was moved

--- settings/device_list_model.cpp
+++ settings/device_list_model.cpp
@@ -1,26 +1,8 @@
 #include "device_list_model.h"
-#include "device_storage.h"
-
 
 DeviceListModel::DeviceListModel(QObject *parent) : QAbstractListModel(parent)
 {
 
-}
-
-int DeviceListModel::rowCount(const QModelIndex &parent) const
-{
-    return devices.size();
-}
-
-QVariant DeviceListModel::data(const QModelIndex &index, int role) const
-{
-    if (!index.isValid())
-        return QVariant();
-
-    if (role == Qt::DisplayRole)
-        return devices.at(index.row())->getName();
-
-    return QVariant();
 }
 
 DeviceListModel::OperationResult DeviceListModel::addDevice(const std::shared_ptr<Device> &newDevice)
@@ -47,4 +29,20 @@ bool DeviceListModel::isUniqueDeviceName(const QString &name)
                         [&name](const std::shared_ptr<const Device>& existingDevice) {
                             return existingDevice->getName() == name;
                         });
+}
+
+int DeviceListModel::rowCount(const QModelIndex &parent) const
+{
+    return devices.size();
+}
+
+QVariant DeviceListModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    if (role == Qt::DisplayRole)
+        return devices.at(index.row())->getName();
+
+    return QVariant();
 }
