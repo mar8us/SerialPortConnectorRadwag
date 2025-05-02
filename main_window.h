67034db882938a41ebf0abad6919be299d72cdb3
mar8us <gordonsix0@gmail.com>
2025-05-02 19:29:33 +0200

    RadwagScaleConnector - temp refactoring DeviceConnector

--- main_window.h
+++ main_window.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include "fluid_tabels/fluid_manager.h"
 #include "material_tabels/material_manager.h"
+#include "radwag/radwag_measure.h"
+#include "radwag/radwag_scale_connector.h"
 #include "sample/sample_manager.h"
 #include "settings/device.h"
 #include "settings/device_connector.h"
@@ -56,6 +58,8 @@ private slots:
 private:
     bool canEditDevice(std::shared_ptr<const Device> &device);
     std::shared_ptr<const Device> getSelectedDevice();
+    void connectDevice();
+    void disconnectDevice();
 
     void initControls();
     void connectButtons();
@@ -84,7 +88,7 @@ private:
     Ui::MainWindow *ui;
     DeviceListModel devicesListModel;
     DeviceControler devicesListControler;
-    DeviceConnector deviceConnector;
+    std::unique_ptr<RadwagScaleConnector> radwagScaleConnector;
     std::unique_ptr<FluidManager> fluidManager;
     std::unique_ptr<MaterialManager> materialManager;
     std::unique_ptr<SampleManager> sampleManager;
@@ -97,3 +101,149 @@ private:
     static inline const QColor ACTIVE_LABEL_COLOR = QColor(0, 0, 255);
 };
 #endif // MAIN_WINDOW_H
+
+
+
+
+
+
+
+
+
+// #ifndef MAIN_WINDOW_H
+// #define MAIN_WINDOW_H
+
+// #include <QMainWindow>
+// #include "fluid_tabels/fluid_manager.h"
+// #include "material_tabels/material_manager.h"
+// #include "radwag/radwag_measure.h"
+// #include "sample/sample_manager.h"
+// #include "settings/device.h"
+// #include "settings/device_connector.h"
+// #include "settings/devices_list_model.h"
+// #include "settings/devices_list_controler.h"
+// #include "radwag/measurement_manager.h"
+// #include "main_widow/measurement_controller.h"
+
+// QT_BEGIN_NAMESPACE
+// namespace Ui {
+// class MainWindow;
+// }
+// QT_END_NAMESPACE
+
+// class MainWindow : public QMainWindow
+// {
+//     Q_OBJECT
+
+// public:
+//     MainWindow(QWidget *parent = nullptr);
+//     ~MainWindow();
+
+// signals:
+//     void beginNewMeasure();
+
+// private slots:
+//     void onAddDeviceButtonClicked();
+//     void onEditDeviceButtonClicked();
+//     void onRemoveDeviceButtonClicked();
+//     void onDeviceComboSelectionChanged();
+//     void onConnectDeviceClicked();
+//     void onDisconnectDeviceClicked();
+//     void onConnectResult(bool connected);
+
+//     void onStartMeasureButtonClicked();
+//     void onShowHydroSetSchemeButtonClicked();
+//     void onConfrimPrepareWorkstationButtonClicked();
+
+//     void onMeasurementTypeChanged();
+//     void onSampleComboBoxChanged(int index);
+
+//     void navigateToToolBoxPage(QWidget* page);
+//     void goToPreviousMeasureStage();
+//     void goToNextMeasureStage();
+//     void onMainPageChanged(int index);
+
+//     void buttonTableFluidsOnClicked();
+//     void buttonSamplesOnClicked();
+
+//     void onMaterialsChanged(const QMap<QString, Material> &materials);
+
+//     void onBeginNewMeasure();
+//     void onGetCurrentDryMeasureButtonClicked();
+//     void onRadwagMeasueReady(const RadwagMeasure &data);
+//     void onSaveCurrentDryMeasureButtonClicked();
+//     void onClearSavedDryMeasureButtonClicked();
+
+//     void onDryMassExecuteStepOneClicked();
+//     void onDryMassExecuteStepTwoClicked();
+//     void onDryMassExecuteStepThreeClicked();
+
+//     void onGetCurrentFinishMeasureSecondButtonClicked();
+//     void onSaveCurrentFinishMeasureSecondButtonClicked();
+//     void onClearSavedFinishMeasureSecondButtonClicked();
+//     void onCalculatelabelVolumeFinishMeasureSecondButtonClicked();
+
+//     void onFinishMeasureSecondStepTwoClicked();
+//     void onFinishMeasureSecondStepTreeClicked();
+
+// private:
+//     bool canEditDevice(std::shared_ptr<const Device> &device);
+//     std::shared_ptr<const Device> getSelectedDevice();
+
+//     void initControls();
+//     void connectButtons();
+//     void connectMainNavButtons();
+//     void connectNavMeasurementButtons();
+//     void connectPrepareWorksationPageButtons();
+//     void connectInitialDataPageButtons();
+//     void connectDryMassPageButtons();
+//     void connectFinishSecondPageButtons();
+//     void connectCatalogsButtons();
+//     void connectDevicesSettingsButtons();
+//     void updateStageLabels();
+//     void finishMeasurement();
+//     void setProperty();
+//     void setIcons();
+//     void fillDevicesCombo(bool keepActiveDevice = false);
+//     void fillSerialPortCombo();
+//     void fillSampleCombo();
+//     void fillFluidCombo();
+//     void updateActionIcons(int index);
+//     void updateStatusConnectionLabel(bool connectionStatus);
+//     void updateConnectonLabelsStatusBar(bool connectionStatus);
+//     void upadteSampleEditors();
+//     void clearSampleEditors();
+
+//     // MeasurementRadwag
+//     void fillPrepareDataLabels();
+//     void fillSampleInfoLabels();
+//     void fillFluidInfoLabels();
+//     void fillTemperatureComboBox();
+//     void updateFluidDensityLabel();
+//     void fillFinishMeasureSecondLabels();
+
+//     bool checkDeviceConnectionWithMessage();
+//     bool validateInitialData();
+//     bool vaildateDryMeasureData();
+
+//     void updateSaveCurrentDryMeasureButtonState();
+//     void updateSaveFinishSecondButtonState();
+
+//     Ui::MainWindow *ui;
+//     DeviceListModel devicesListModel;
+//     DeviceControler devicesListControler;
+//     DeviceConnector deviceConnector;
+//     std::unique_ptr<FluidManager> fluidManager;
+//     std::unique_ptr<MaterialManager> materialManager;
+//     std::unique_ptr<SampleManager> sampleManager;
+//     std::shared_ptr<MeasurementManager> measurememntManager;
+//     std::unique_ptr<MeasurementController> radwagMeasureControler;
+
+//     QIcon defaultSettingsIcon;
+//     QIcon activeSettingsIcon;
+//     QIcon defaultRadwagIcon;
+//     QIcon activeRadwagIcon;
+
+//     static inline const QColor ACTIVE_LABEL_COLOR = QColor(0, 0, 255);
+// };
+// #endif // MAIN_WINDOW_H
--- radwag/radwag_measure.cpp
+++ radwag/radwag_measure.cpp
@@ -1,6 +1,197 @@
 #include "radwag_measure.h"
+#include <QRegularExpression>
 
-RadwagMeasure::RadwagMeasure(const QByteArray& rawData) : DeviceData(rawData)
+
+RadwagMeasure::RadwagMeasure(const QByteArray& rawData)
+    : DeviceData(rawData), _value(0.0), _unitStr(""), _unit(Unit::Unknown), _stable(false)
 {
     parse();
 }
+
+RadwagMeasure::~RadwagMeasure()
+{
+
+}
+
+bool RadwagMeasure::parse()
+{
+    if(rawData.isEmpty())
+        return false;
+
+    QString data = QString::fromLatin1(rawData).trimmed();
+
+    if(parseFlexibly(data))
+        return true;
+
+    return parseStrictFormat(rawData);
+}
+
+bool RadwagMeasure::parseFlexibly(const QString &data)
+{
+    QRegularExpression rxFloat("([+-]?\\d+\\.\\d+)");
+    QRegularExpressionMatch match = rxFloat.match(data);
+
+    if(match.hasMatch())
+    {
+        QString valueStr = match.captured(1);
+        bool conversionOk;
+        double parsedValue = valueStr.toDouble(&conversionOk);
+        if(!conversionOk)
+            return false;
+        _value = parsedValue;
+
+        // Znajdź jednostkę - zwykle zaraz po wartości liczbowej
+        int valuePos = data.indexOf(valueStr) + valueStr.length();
+
+        // Pomiń białe znaki
+        int unitStart = valuePos;
+        while(unitStart < data.length() && data.at(unitStart).isSpace())
+            unitStart++;
+
+        // Pobierz znaki jednostki (do 3 znaków lub do białego znaku)
+        if(unitStart < data.length())
+        {
+            int unitEnd = unitStart;
+            int maxUnitChars = qMin(3, data.length() - unitStart);
+            for(int i = 0; i < maxUnitChars; i++)
+            {
+                if(data.at(unitStart + i).isSpace() || data.at(unitStart + i) == '\r' || data.at(unitStart + i) == '\n')
+                    break;
+                unitEnd++;
+            }
+            _unitStr = data.mid(unitStart, unitEnd - unitStart);
+            _unit = parseUnitFromString(_unitStr);
+        }
+
+        // Sprawdź stabilność (brak znaku zapytania oznacza stabilność)
+        _stable = !data.contains("?");
+        return true;
+    }
+    return false;
+}
+
+bool RadwagMeasure::parseStrictFormat(const QByteArray &data)
+{
+    // Format według dokumentacji:
+    // 1 – 3 4 5 6 7 8 – 16 17 18 - 20 21 22
+    // Rozkaz spacja znak_stabilności spacja znak Masa spacja jednostka CRLF
+
+    if (data.size() < 14)
+        return false;
+
+    _stable = (data.at(4) == ' '); // spacja oznacza stabilny, '?' oznacza niestabilny
+
+    // Pomijamy rozkaz i znaki kontrolne, szukamy wartości masowej
+    // Typowo masa jest w pozycjach 8-16
+    bool foundValue = false;
+
+    for(int i = 6; i < data.size() - 5; i++)
+    {
+        if(isdigit(data.at(i)) || data.at(i) == '.')
+        {
+            // Szukaj końca liczby
+            int j = i;
+            while(j < data.size() && (isdigit(data.at(j)) || data.at(j) == '.'))
+                j++;
+
+            QString massStr = QString::fromLatin1(data.mid(i, j-i));
+            bool conversionOk;
+            double parsedValue = massStr.toDouble(&conversionOk);
+
+            if (conversionOk)
+            {
+                _value = parsedValue;
+                foundValue = true;
+
+                // Szukaj jednostki po wartości liczbowej
+                int unitPos = j;
+                while(unitPos < data.size() && data.at(unitPos) == ' ')
+                    unitPos++;
+
+                // Pobierz jednostkę (do 3 znaków)
+                int unitEnd = unitPos;
+                while(unitEnd < qMin(unitPos + 3, data.size()) &&
+                       data.at(unitEnd) != ' ' &&
+                       data.at(unitEnd) != '\r' &&
+                       data.at(unitEnd) != '\n')
+                {
+                    unitEnd++;
+                }
+
+                _unitStr = QString::fromLatin1(data.mid(unitPos, unitEnd - unitPos));
+                _unit = parseUnitFromString(_unitStr);
+                break;
+            }
+        }
+    }
+
+    return foundValue;
+}
+
+RadwagMeasure::Unit RadwagMeasure::parseUnitFromString(const QString &unitStr)
+{
+    QString unit = unitStr.trimmed().toLower();
+
+    if(unit == "g")
+        return Unit::G;
+    if(unit == "kg")
+        return Unit::KG;
+    if(unit == "ct")
+        return Unit::CT;
+    if(unit == "lb")
+        return Unit::LB;
+    if(unit == "oz")
+        return Unit::OZ;
+
+    return Unit::Unknown;
+}
+
+// Gettery i settery
+double RadwagMeasure::getValue() const
+{
+    return _value;
+}
+
+void RadwagMeasure::setValue(double value)
+{
+    _value = value;
+}
+
+QString RadwagMeasure::getUnitString() const
+{
+    return _unitStr;
+}
+
+void RadwagMeasure::setUnitString(const QString &unitStr)
+{
+    _unitStr = unitStr;
+    _unit = parseUnitFromString(unitStr);
+}
+
+RadwagMeasure::Unit RadwagMeasure::getUnit() const
+{
+    return _unit;
+}
+
+void RadwagMeasure::setUnit(Unit unit)
+{
+    _unit = unit;
+}
+
+bool RadwagMeasure::isStable() const
+{
+    return _stable;
+}
+
+void RadwagMeasure::setStable(bool stable)
+{
+    _stable = stable;
+}
+
+
+// #include "radwag_measure.h"
+
+// RadwagMeasure::RadwagMeasure(const QByteArray& rawData) : DeviceData(rawData)
+// {
+//     parse();
+// }
