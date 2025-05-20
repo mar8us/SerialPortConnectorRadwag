369864d7b3eb9bf3c41a13cb339158543c28e261
mar8us <gordonsix0@gmail.com>
2025-05-20 23:53:30 +0200

    Prepare saturation page - connect buttonts and filling labels

--- utils.cpp
+++ utils.cpp
@@ -15,4 +15,34 @@ double getDouble(const QString &text)
     return conversionOk ? value : 0.0;
 }
 
+SaturationMethod getSaturationMethodFromIndex(int index)
+{
+    switch (index)
+    {
+        case 0:
+            return SaturationMethod::BoilingInWater;
+        case 1:
+            return SaturationMethod::VacuumMethod;
+        case 2:
+            return SaturationMethod::LongTermSoaking;
+        default:
+            return SaturationMethod::BoilingInWater;
+    }
+}
+
+QString getSaturationMethodName(SaturationMethod method)
+{
+    switch (method)
+    {
+        case SaturationMethod::BoilingInWater:
+            return "Gotowanie w wodzie";
+        case SaturationMethod::VacuumMethod:
+            return "Próżniowa";
+        case SaturationMethod::LongTermSoaking:
+            return "Długotrwałe nasączanie";
+        default:
+            return "Nieznana metoda";
+    }
+}
+
 }
--- utils.h
+++ utils.h
@@ -1,11 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include "radwag/measurement.h"
 #include <QString>
 
 namespace utils
 {
 
 double getDouble(const QString &text);
+QString getSaturationMethodName(SaturationMethod method);
+SaturationMethod getSaturationMethodFromIndex(int index);
 
 }
 
